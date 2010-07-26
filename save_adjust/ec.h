2c560f589ac6126f7f96998aac716df3400789a6
booto <remornicus@gmail.com>
2010-07-27 01:19:14 +0800

    initial save_adjust commit

---
+++ save_adjust/ec.h
@@ -0,0 +1,14 @@
+// Copyright 2010  booto 
+// Licensed under the terms of the GNU GPL, version 2
+// http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+#ifndef ec_h
+#define ec_h
+
+#include "types.h"
+
+int check_ec(u8 *ng, u8 *ap, u8 *sig, u8 *sig_hash);
+void ec_priv_to_pub(u8 *k, u8 *Q);
+int check_ecdsa(u8 *Q, u8 *R, u8 *S, u8 *hash);
+int generate_ecdsa(u8 *R, u8 *S, u8 *k, u8 *hash);
+
+#endif
